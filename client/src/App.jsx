32486c5d9f1488457ddb005b97e4e57927d37ea6
Gadiraju Yasaswini Devi <g.yasaswini2023@gmail.com>
2025-04-28 23:12:06 +0530

    Modifying the App.jsx to route to login and signup pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ExpenseTracker from "./components/ExpenseTracker";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="container mt-4">
-      <header className="text-center mb-5">
-        <h1 className="fw-bold text-primary">Expense Tracker</h1>
-      </header>
-      <ExpenseTracker />
-    </div>
+    <Router>
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<ExpenseTracker />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
