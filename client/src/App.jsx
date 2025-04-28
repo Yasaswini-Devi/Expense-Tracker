c9bdd673bceab43b232944941d25676bf30e39e7
Gadiraju Yasaswini Devi <g.yasaswini2023@gmail.com>
2025-04-28 23:45:32 +0530

    Adding the route to home page in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ExpenseTracker from "./components/ExpenseTracker";
+import ExpenseTracker from "./components/dashboard/ExpenseTracker";
 import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
+import Home from "./components/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -10,9 +11,10 @@ function App() {
     <Router>
       <div className="container mt-4">
         <Routes>
-          <Route path="/" element={<ExpenseTracker />} />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<ExpenseTracker />} />
         </Routes>
       </div>
     </Router>
