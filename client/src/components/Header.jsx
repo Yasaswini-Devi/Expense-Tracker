5747b7030b00aedd22c31a96d8c4cc5ae6db17d3
Gadiraju Yasaswini Devi <g.yasaswini2023@gmail.com>
2025-04-29 19:42:38 +0530

    Added the header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Header = () => {
+  return (
+    <nav className="header navbar navbar-expand-lg shadow-sm">
+      <div className="container">
+        {/* Logo Section */}
+        <Link className="navbar-brand text-white" to="/">
+          <strong>Expense Tracker</strong>
+        </Link>
+
+        {/* Toggle Button for Mobile */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Navigation Links */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/login">
+                Login
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/signup">
+                Signup
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
